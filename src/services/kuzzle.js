e8302cb503a9865c9b93b847180a6bcc3374a4a3
Luca Marchesini <xbill82@gmail.com>
2016-06-16 08:32:55 +0200

    Added Kuzzle service

---
+++ src/services/kuzzle.js
@@ -0,0 +1,11 @@
+import Kuzzle from 'kuzzle-sdk'
+import Config from '../config'
+
+/** TODO - Step 1: initialize a Kuzzle object */
+var kuzzle = new Kuzzle(Config.kuzzleUrl, {defaultIndex: Config.defaultIndex});
+
+export default kuzzle;
+
+export function initSubscriptions() {
+  
+}
